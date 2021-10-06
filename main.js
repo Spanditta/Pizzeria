c54b0e0d12d551c2983ac149a1203e423f85e3fc
Spanditta <84282823+Spanditta@users.noreply.github.com>
2021-10-06 17:15:04 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+
+
+
+
+
+
+
+menu_list_array = ["Veg Margherita Pizza","Chicken Sausage Pizza","Ghost Pizza", "Double Cheese Pizza", "Chicken Tikka Pizza",
+ "Corn Pizza", "Bahubali Pizza",];
+
+function getmenu(menu_list_array){
+var htmldata;
+
+
+        for(var i=0;i<menu_list_array.length;i++){
+            htmldata=htmldata+ menu_list_array[i] + '<br>'
+        }
+        document.getElementById("display_menu").innerHTML = htmldata;
+        //give the appropriate id name as display_menu  
+}
+
+function add_item(){
+var htmldata;
+var item=document.getElementById("add_item").value;
+menu_list_array.sort();
+        htmldata=""
+        for(var i=0;i<menu_list_array.length;i++){
+            htmldata=htmldata+imgtags+ menu_list_array[i]+'<br>';
+        }
+         document.getElementById("display_addedmenu").innerHTML = htmldata;		
+      }
+
+function add_top(){
+    var item=document.getElementById("add_item").value;
+    menu_list_array.push(item);
+	add_item();
+}
